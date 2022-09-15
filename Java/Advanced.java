b2d759837b5e086b9e59d2b4b68fb245aed27541
00eunyoung <66937214+00eunyoung@users.noreply.github.com>
2022-09-15 11:08:01 +0900

    Create Advanced.java

---
+++ Java/Advanced.java
@@ -0,0 +1,13 @@
+import mypackage.Basic;
+
+public class Advanced extends Basic {
+	public void show() {
+		
+		System.out.println(o);
+		System.out.println(getp());
+		System.out.println(getq());
+		System.out.println(r);
+	
+
+}
+}
